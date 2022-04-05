8e8b13d7e46d7cd753375329ed1ea4fcc3f247f9
duongmoe174 <duongmoe174@gmail.com>
2022-04-05 16:58:59 +0700

    2

--- src/main/java/com/team4/service/IService.java
+++ src/main/java/com/team4/service/IService.java
@@ -1,4 +1,12 @@
 package com.team4.service;
 
-public interface IService {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IService<T>{
+    List<T> selectAll();
+    void insert(T t);
+    T getById (int id);
+    boolean delete (int id) throws SQLException;
+    boolean update (T t) throws SQLException;
 }
---
+++ src/main/java/com/team4/service/user/IUserService.java
@@ -0,0 +1,4 @@
+package com.team4.service.user;
+
+public interface IUserService {
+}
